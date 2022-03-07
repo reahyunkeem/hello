d7ad665537dd491e2519ed117e839c8b4fe9e4d6
reahyunkeem <rlafogus@gmail.com>
2022-03-07 14:23:50 +0900

    예제3

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var port = 8080;
 http.createServer( (req, res) => {
     console.log('Requested: ' + req.url);
     res.writeHead(200, { 'Content-Type':'text/html; charset=utf-8' }); 
-    res.write('<h1>docker 예제 2</h1>');
+    res.write('<h1>docker 예제 3</h1>');
     res.write('<h2>Hello node!</h2>');
     res.end(); }
 ).listen(port, () => {
